b81610d93f013a981fc68e4e83cd936ddb7a0648
Alexander Ayers <alexayers2000@gmail.com>
2022-04-24 14:16:27 -0400

    Added gui fixes.
    Changed values in GUI to make boxes smaller and closer together.
    Moved methods in handleKeyDown into private helper methods.
    Added documentation to constructor in WordleMainWindow.

--- view/WordleMainWindow.cpp
+++ view/WordleMainWindow.cpp
@@ -1,33 +1,43 @@
 #include "WordleMainWindow.h"
 
-#include <iostream>
 #include <string>
 using namespace std;
 
 namespace view
 {
+
+/**
+    Creates an instance of WordleMainWindow with width, height, and title.
+
+    precondition: None
+    postcondition: All values have been set.
+
+    Params:
+        width - The width of the application.
+        height - The height of the application.
+        title - The title of the application.
+ */
 WordleMainWindow::WordleMainWindow(int width, int height, const char* title) : Fl_Window(width, height, title)
 {
     this->currentRow = 0;
     this->currentColumn = 0;
     begin();
-    int boxYPosition = 25;
-    int boxSquareSize = 40;
-    int xIncrement = boxSquareSize + 15;
-    int yIncrement = boxSquareSize + 20;
+    int boxYPosition = 20;
+    int boxSquareSize = 30;
+    int xIncrement = boxSquareSize + 5;
+    int yIncrement = boxSquareSize + 10;
     for (int i = 0; i < WordleMainWindow::NUMBER_OF_ROWS; ++i)
     {
-        int boxXPosition = 100;
+        int boxXPosition = 50;
         for (int j = 0; j < WordleMainWindow::NUMBER_OF_COLUMNS; ++j)
         {
-            Fl_Box* box = new Fl_Box(boxXPosition, boxYPosition, boxSquareSize, boxSquareSize, "A");
+            Fl_Box* box = new Fl_Box(boxXPosition, boxYPosition, boxSquareSize, boxSquareSize, "");
             box->box (FL_UP_BOX);
             this->boxes[i][j] = box;
             boxXPosition += xIncrement;
         }
         boxYPosition += yIncrement;
     }
-
 
     end();
 }
@@ -37,11 +47,11 @@ int WordleMainWindow::handle(int event)
     switch(event)
     {
     case FL_KEYDOWN:
-        return handleKeyDown(event,Fl::event_key());
+        return handleKeyDown(Fl::event_key());
     }
 }
 
-int WordleMainWindow::handleKeyDown(int event, int key)
+int WordleMainWindow::handleKeyDown(int key)
 {
     if (key == FL_Enter)
     {
@@ -49,7 +59,17 @@ int WordleMainWindow::handleKeyDown(int event, int key)
     }
     if (key == FL_BackSpace)
     {
-        if (this->currentColumn > -1)
+        handleBackspace();
+    }
+    if (key >= 'a' && key <= 'z')
+    {
+        handleLetterKeyPress(key);
+    }
+}
+
+int WordleMainWindow::handleBackspace()
+{
+    if (this->currentColumn > -1)
         {
             this->getCurrentBox()->label("");
 
@@ -59,10 +79,11 @@ int WordleMainWindow::handleKeyDown(int event, int key)
                 this->currentColumn = 0;
             }
         }
-    }
-    if (key >= 'a' && key <= 'z')
-    {
-        if (this->currentColumn < WordleMainWindow::NUMBER_OF_COLUMNS)
+}
+
+int WordleMainWindow::handleLetterKeyPress(int key)
+{
+    if (this->currentColumn < WordleMainWindow::NUMBER_OF_COLUMNS)
         {
             char output[1];
             output[0] = (char) key;
@@ -73,7 +94,6 @@ int WordleMainWindow::handleKeyDown(int event, int key)
                 this->currentColumn = WordleMainWindow::NUMBER_OF_COLUMNS - 1;
             }
         }
-    }
 }
 
 Fl_Box* WordleMainWindow::getCurrentBox()
@@ -81,6 +101,12 @@ Fl_Box* WordleMainWindow::getCurrentBox()
     return this->boxes[this->currentRow][this->currentColumn];
 }
 
+/**
+    Deletes this instance of WordleMainWindow.
+
+    precondition: None
+    postcondition: All values have been deleted.
+ */
 WordleMainWindow::~WordleMainWindow()
 {
     for (int i = 0; i < WordleMainWindow::NUMBER_OF_ROWS; ++i)
