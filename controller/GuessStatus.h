e8c9b77e935d117a94dd3a6179ffffa53c3308b2
ghoststorm34 <alexayers2000@gmail.com>
2022-04-25 18:37:58 -0400

    Readded controller and GuessStatus
    Due to a Git error. the controller and GuessStatus were deleted. They have been readded now.

---
+++ controller/GuessStatus.h
@@ -0,0 +1,16 @@
+#ifndef GUESSSTATUS_H
+#define GUESSSTATUS_H
+
+namespace controller
+{
+
+enum GuessStatus
+{
+    DOES_NOT_EXIST,
+    INCORRECT_POSITION,
+    CORRECT_POSITION,
+};
+
+}
+
+#endif // GUESSSTATUS_H
