2d82f6e4e90aa5b4f73115f642dddb62eee67a6b
ghoststorm34 <alexayers2000@gmail.com>
2022-05-01 13:23:12 -0400

    Added color values to ApplicationColors
    Added in the values for the status colors that correspond to the different varieties of color blindness.

--- utils/ApplicationColors.h
+++ utils/ApplicationColors.h
@@ -1,5 +1,7 @@
 #ifndef APPLICATIONCOLORS_H
 #define APPLICATIONCOLORS_H
+#define NUMBER_OF_COLOR_BLINDNESS_OPTIONS 4
+#define NUMBER_OF_APPLICATION_COLORS 4
 
 /**
 Data pertaining to the colors of the application.
@@ -22,7 +24,15 @@ public:
         PROTANOPIA,
         TRITANOPIA
     };
-    const string& ColorBlindessText[COLOR_BLINDNESS_COUNT] = {"Normal Vision", "Deuteranopia", "Protanopia", "Tritanopia"};
+    static const string& COLOR_BLINDNESS_TEXT[NUMBER_OF_COLOR_BLINDNESS_OPTIONS] = {"Normal Vision", "Deuteranopia", "Protanopia", "Tritanopia"};
+    static const int[NUMBER_OF_COLOR_BLINDNESS_OPTIONS][NUMBER_OF_APPLICATION_COLORS] STATUS_COLORS = {NORMAL_VISION_COLORS, DEUTERANOPIA_COLORS, PROTANOPIA_COLORS, TRITANOPIA_COLORS}
+
+private:
+    static const int NORMAL_VISION_COLORS[NUMBER_OF_APPLICATION_COLORS] = {49, 45, 95, 63}
+    static const int DEUTERANOPIA_COLORS[NUMBER_OF_APPLICATION_COLORS] = {49, 45, 124, 175}
+    static const int PROTANOPIA_COLORS[NUMBER_OF_APPLICATION_COLORS] = {49, 45, 84, 95}
+    static const int TRITANOPIA_COLORS[NUMBER_OF_APPLICATION_COLORS] = {49, 45, 253, 191}
+
 
 
 };
